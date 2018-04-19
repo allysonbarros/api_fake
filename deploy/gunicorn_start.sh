c8942a99c5a140090c20fa9be383daa523fa007f
Allyson Barros <allysonbarrosrn@gmail.comm>
2018-04-19 10:53:51 -0300

    Ajustes gerais.

--- deploy/gunicorn_start.sh
+++ deploy/gunicorn_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-LOGFILE=/var/log/api_fake/gunicorn.log
+LOGFILE=/home/vagrant/api_fake/deploy/gunicorn.log
 LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=2 # idealmente deve ser 2n + 1 (n = qtd de processadores)
 USER=www-data
@@ -10,7 +10,7 @@ test -d $LOGDIR || mkdir -p $LOGDIR
 . /etc/default/locale
 export LANG
 export LC_ALL
-exec /var/.virtualenvs/api_fake/bin/gunicorn -w $NUM_WORKERS \
+exec /var/.virtualenvs/api_fake/bin/gunicorn api_faker.wsgi -w $NUM_WORKERS \
   --user=$USER --group=$GROUP --log-level=debug \
   --log-file=$LOGFILE 2>>$LOGFILE \
   --timeout=1800
--- deploy/install.sh
+++ deploy/install.sh
@@ -6,16 +6,16 @@ export LC_ALL="pt_br.UTF-8"
 sudo locale-gen pt_br.UTF-8
 
 echo "updating the package manager"
-apt-get update -q
+sudo apt-get update -q
 
 echo "installing dependencies available via apt-get"
-apt-get install -y -q build-essential git python-dev python-setuptools nginx supervisor
+sudo apt-get install -y -q build-essential git python-dev python-setuptools nginx supervisor
 
 echo "installing pip"
-easy_install pip
+sudo easy_install pip
 
 echo "installing python dependencies available via pip"
-pip install virtualenv
+sudo pip install virtualenv
 
 echo "creating a new python virtualenv"
 virtualenv /var/.virtualenvs/api_fake
@@ -23,3 +23,21 @@ source /var/.virtualenvs/api_fake/bin/activate
 
 echo "installing dependencies defined in requirements.txt"
 pip install -r requirements.txt
+
+echo "creating logs' folder"
+mkdir deploy/logs
+
+echo "changing owner of deploy's folder"
+sudo chown -R www-data:www-data deploy
+
+echo "copying nginx's configuration file"
+sudo cp deploy/api_fake.nginx /etc/nginx/sites-enabled/
+
+echo "restarting nginx"
+sudo service nginx restart
+
+echo "copying supervisor's configuration file"
+sudo cp deploy/api_fake.conf.supervisor /etc/supervisor/conf.d/
+
+echo "restarting supervisor"
+sudo service supervisor restart
